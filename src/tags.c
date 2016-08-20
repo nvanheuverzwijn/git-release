5cffe1e61a0c93f1ca0846866fe44bd0b34e325b
Nicolas Vanheuverzwijn <nicolas.vanheu@gmail.com>
2016-08-20 15:38:57 -0400

    fix: memory leak

--- src/tags.c
+++ src/tags.c
@@ -1,9 +1,12 @@
 #include <git2.h>
+#include <string.h>
 #include "tags.h"
 #include "errors.h"
+#include "memory.h"
 
 int git_release_tag_get_last(git_repository *repo, char** out)
 {
+	char* last_tag = NULL;
 	git_strarray tags = {0};
 	if(git_tag_list(&tags, repo))
 	{
@@ -11,8 +14,15 @@ int git_release_tag_get_last(git_repository *repo, char** out)
 	}
 	if(tags.count == 0)
 	{
+		git_strarray_free(&tags);
 		return E_TAGNOTFOUND;
 	}
-	*out = tags.strings[tags.count - 1];
+	last_tag = tags.strings[tags.count - 1];
+
+	*out = xmalloc(strlen(last_tag));
+	strncpy(*out, last_tag, strlen(last_tag));
+
+	git_strarray_free(&tags);
+
 	return 0;
 }
