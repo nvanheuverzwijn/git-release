539cf4d8d88f0aac5b73a7e6bb76818da197a14b
Nicolas Vanheuverzwijn <nicolas.vanheu@gmail.com>
2016-09-03 17:16:26 -0400

    feat: add substr function

--- src/string_utility.c
+++ src/string_utility.c
@@ -1,5 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include "string_utility.h"
+#include "memory.h"
 
 int git_release_string_utility_endswith(const char *string, const char *suffix)
 {
@@ -9,9 +12,30 @@ int git_release_string_utility_endswith(const char *string, const char *suffix)
 	}
 	size_t stringlen = strlen(string);
 	size_t suffixlen = strlen(suffix);
-    if(suffixlen >  stringlen)
+	if(suffixlen > stringlen)
 	{
 		return 0;
 	}
 	return strncmp(string + stringlen - suffixlen, suffix, suffixlen) == 0;
+}
+
+int git_release_string_utility_substr(const char* source, int len, char** out)
+{
+	size_t source_len = strlen(source);
+	if(source_len < abs(len))
+	{
+		return 1;
+	}
+	size_t out_len = source_len - abs(len);
+	*out = xmalloc(out_len);
+	if(len > 0)
+	{
+		memcpy(*out, source + len, out_len);
+	}
+	else if(len <= 0)
+	{
+		memcpy(*out, source, out_len);
+	}
+	(*out)[out_len] = '\0';
+	return 0;
 }
--- src/string_utility.h
+++ src/string_utility.h
@@ -2,5 +2,6 @@
 #define STRING_UTILITY_H
 
 int git_release_string_utility_endswith(const char *str, const char *suffix);
+int git_release_string_utility_substr(const char* source, int len, char** out);
 
 #endif
